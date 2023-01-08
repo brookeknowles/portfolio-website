c98f51701b6224fb7bf39ef2849b5aec5357c4c9
Brooke Knowles <62309663+brookeknowles@users.noreply.github.com>
2023-01-08 19:48:49 +1300

    add intro section

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function Intro(){
     return(
-        <div>
-            Intro
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className="text-4x md:text-7xl mb-1 md:mb-3 font-bold">Brooke Knowles</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Computer Engineering Student & Software Developer</p>
+            <p className="text-small max-w-xl mb-6 font-bold">I am a final year student at the University of Auckland,
+        finishing off my Bachelor of Engineering (Honours) degree, specialised in Computer Systems Engineering. <br /> More goes here I guess</p>
         </div>
     )
 }
